4ccb76c1ce874853118ed96aadea3834785cc003
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2021-09-13 16:11:01 -0300

    ArvoreGenealogicaDaFamiliaPinheiro

--- Árvore Genealógica daFamília Pinheiro/pinheiro.pl
+++ Árvore Genealógica daFamília Pinheiro/pinheiro.pl
@@ -20,60 +20,44 @@ sexo(joao,masculino).
 sexo(jose,masculino).
 sexo(carlos,masculino).
 
-irma(X,Y):- progenitor(A,X),
- progenitor(A,Y),
- X\==Y,
- sexo(X,feminino).
+irma(X,Y):- progenitor(A,X), progenitor(A,Y), X\==Y, sexo(X,feminino).
 
-irmao(X,Y):- progenitor(A,X),
- progenitor(A,Y),
- X\==Y,
- sexo(X,masculino).
+irmao(X,Y):- progenitor(A,X), progenitor(A,Y), X\==Y, sexo(X,masculino).
 
 descendente(X,Y):- progenitor(X,Y).
-descendente(X,Y):- progenitor(X,A),
- descendente(A,Y).
+descendente(X,Y):- progenitor(X,A), descendente(A,Y).
 
-avo(X,Y):- progenitor(X,A),
- progenitor(A,Y),
- sexo(X,masculino).
+avo(X,Y):- progenitor(X,A), progenitor(A,Y), sexo(X,masculino).
 
-mae(X,Y):- progenitor(X,Y),
- sexo(X,feminino).
+mae(X,Y):- progenitor(X,Y), sexo(X,feminino).
 
-pai(X,Y):- progenitor(X,Y),
- sexo(X,masculino).
+pai(X,Y):- progenitor(X,Y), sexo(X,masculino).
 
-tio(X,Y):- irmao(X,A),
- progenitor(A,Y).
+tio(X,Y):- irmao(X,A), progenitor(A,Y).
 
-primo(X,Y):-irmao(A,B),
- progenitor(A,X),
- progenitor(B,Y),
- X\==Y.
+primo(X,Y):-irmao(A,B), progenitor(A,X), progenitor(B,Y), X\==Y.
 
-primo(X,Y):-irma(A,B),
- progenitor(A,X),
- progenitor(B,Y),
- X\==Y.
+primo(X,Y):-irma(A,B), progenitor(A,X), progenitor(B,Y), X\==Y.
 
 % questoes:
 
-q1:- progenitor(jose,joao).
-q1b:- pai(jose,joao).
+q1:- progenitor(jose,joao). % true
+q1b:- pai(jose,joao). % true
 
-q2(X):- mae(maria,X).
-q2b(L):-findall(X,mae(maria,X),L).
+q2(X):- mae(maria,X). % X = joao X = ana
+q2b(L):-findall(X,mae(maria,X),L). % L = [joao, ana]
 
-q3(X):- primo(mario,X).
-q3b(L):- findall(X,primo(mario,X),L).
-q3c(L):- findall(X,primo(mario,X),LR),list_to_set(LR,L).
+q3(X):- primo(mario,X). % X = helena X = joana 
+q3b(L):- findall(X,primo(mario,X),L). % L = [helena, joana, helena, joana]
+q3c(L):- findall(X,primo(mario,X),LR),list_to_set(LR,L). % L = [helena, joana],
+% LR = [helena, joana, helena, joana]
 
-q4(X):- tio(_,X).
-q4b(L):- findall(X,tio(_,X),LR),list_to_set(LR,L).
+q4(X):- tio(_,X). % X = helena X = joana 
+q4b(L):- findall(X,tio(_,X),LR),list_to_set(LR,L). % L = [helena, joana],
+% LR = [helena, joana, helena, joana]
 
-q5(X):- descendente(X,carlos).
-q5b(L):- findall(X,descendente(X,carlos),L).
+q5(X):- descendente(X,carlos). % X = helena X = mario X = maria X = jose X = maria X = jose X = joao X = ana
+q5b(L):- findall(X,descendente(X,carlos),L). % L = [helena, mario, maria, jose, maria, jose, joao, ana]
 
-q6a(X):- irmao(helena,X).
-q6b(X):- irma(helena,X).
+q6a(X):- irmao(helena,X). % false
+q6b(X):- irma(helena,X). % joana
